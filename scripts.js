7cdb9565cd502f5c4d86376f81fd69338535b074
Kindred <KDSM@protonmail.com>
2022-06-23 14:30:30 +0100

    background change (once again)

--- scripts.js
+++ scripts.js
@@ -1,31 +1,31 @@
-// const options = {};
+const options = {};
 
-// const observer = new IntersectionObserver(function (entries, observer) {
-//   entries.forEach((entry) => {
-//     if (!entry.isIntersecting) {
-//       return;
-//     }
-//     console.log(entry.target);
-//     entry.target.classList.toggle("fade");
-//     observer.unobserve(entry.target);
-//   });
-// }, options);
+const observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    }
+    console.log(entry.target);
+    entry.target.classList.toggle("fade");
+    observer.unobserve(entry.target);
+  });
+}, options);
 
-// observer.observe(home__container);
-// observer.observe(services);
+observer.observe(home__container);
+observer.observe(services);
 
-// const observer2 = new IntersectionObserver(function (entries, observer) {
-//   entries.forEach((entry) => {
-//     if (!entry.isIntersecting) {
-//       return;
-//     }
-//     console.log(entry.target);
-//     entry.target.classList.toggle("fade2");
-//     observer.unobserve(entry.target);
-//   });
-// }, options);
+const observer2 = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    }
+    console.log(entry.target);
+    entry.target.classList.toggle("fade2");
+    observer.unobserve(entry.target);
+  });
+}, options);
 
-// observer2.observe(about__header);
-// observer2.observe(about__description);
+observer2.observe(about__header);
+observer2.observe(about__description);
 
 // MOBILE
