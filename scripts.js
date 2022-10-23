dd538179c70f913af0baded7f4e0a1b7c0dc9b94
Kindred <KDSM@protonmail.com>
2022-10-23 20:01:38 +0100

    markup edits

--- scripts.js
+++ scripts.js
@@ -14,18 +14,18 @@ const observer = new IntersectionObserver(function (entries, observer) {
 observer.observe(home__container);
 observer.observe(services);
 
-const observer2 = new IntersectionObserver(function (entries, observer) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    }
-    console.log(entry.target);
-    entry.target.classList.toggle("fade2");
-    observer.unobserve(entry.target);
-  });
-}, options);
+// const observer2 = new IntersectionObserver(function (entries, observer) {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) {
+//       return;
+//     }
+//     console.log(entry.target);
+//     entry.target.classList.toggle("fade2");
+//     observer.unobserve(entry.target);
+//   });
+// }, options);
 
-observer2.observe(about__header);
-observer2.observe(about__description);
+// observer2.observe(about__header);
+// observer2.observe(about__description);
 
 // MOBILE
