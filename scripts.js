cb1a83f4634d166177a91534c85e8bbda200a8ba
Kindred <KDSM@protonmail.com>
2022-10-24 15:24:35 +0100

    markup edits

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
-const options = {};
+// const options = {};
 
-const observer = new IntersectionObserver(function (entries, observer) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    }
-    console.log(entry.target);
-    entry.target.classList.toggle("fade");
-    observer.unobserve(entry.target);
-  });
-}, options);
+// const observer = new IntersectionObserver(function (entries, observer) {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) {
+//       return;
+//     }
+//     console.log(entry.target);
+//     entry.target.classList.toggle("fade");
+//     observer.unobserve(entry.target);
+//   });
+// }, options);
 
-observer.observe(home__container);
-observer.observe(services);
+// observer.observe(home__container);
+// observer.observe(services);
 
 // const observer2 = new IntersectionObserver(function (entries, observer) {
 //   entries.forEach((entry) => {
