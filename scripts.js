3f05b7fdb8d04758e11b120561e715d93af49e15
Kindred <KDSM@protonmail.com>
2022-10-25 22:00:34 +0100

    added GSAP

--- scripts.js
+++ scripts.js
@@ -1,31 +1,23 @@
-// const options = {};
+gsap.registerPlugin(ScrollTrigger);
 
-// const observer = new IntersectionObserver(function (entries, observer) {
-//   entries.forEach((entry) => {
-//     if (!entry.isIntersecting) {
-//       return;
-//     }
-//     console.log(entry.target);
-//     entry.target.classList.toggle("fade");
-//     observer.unobserve(entry.target);
-//   });
-// }, options);
-
-// observer.observe(home__container);
-// observer.observe(services);
-
-// const observer2 = new IntersectionObserver(function (entries, observer) {
-//   entries.forEach((entry) => {
-//     if (!entry.isIntersecting) {
-//       return;
-//     }
-//     console.log(entry.target);
-//     entry.target.classList.toggle("fade2");
-//     observer.unobserve(entry.target);
-//   });
-// }, options);
-
-// observer2.observe(about__header);
-// observer2.observe(about__description);
-
-// MOBILE
+gsap.from(".hero__container", {
+  scrollTrigger: ".hero__container",
+  duration: 0.8,
+  y: 30,
+  opacity: 0,
+  ease: "in",
+});
+gsap.from(".about__container", {
+  scrollTrigger: ".about__container",
+  duration: 0.8,
+  x: 30,
+  opacity: 0,
+  ease: "in",
+});
+gsap.from(".services__wrapper, .services__header", {
+  scrollTrigger: ".services__header",
+  duration: 0.8,
+  x: -30,
+  opacity: 0,
+  ease: "in",
+});
